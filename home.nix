7537241b3e5ee915b1053eeb60a9690f1a1eaf38
Martin Keefe <budha57@hotmail.com>
2023-09-16 19:25:31 +0100

    Started git and fish config.

--- home.nix
+++ home.nix
@@ -1,6 +1,25 @@
 {pkgs, ...}: {
     home.username = "martin";
     home.homeDirectory = /home/martin;
+    
+    home.packages = with pkgs; [
+        neofetch
+    ];
+    
     home.stateVersion = "22.05";
-    programs.home-manager.enable = true;
+
+    programs = {
+        home-manager.enable = true;
+
+        git = {
+            enable = true;
+
+            userName = "Martin Keefe";
+            userEmail = "budha57@hotmail.com";
+        };
+
+        fish = {
+            enable = true;
+        };
+    };
 }
